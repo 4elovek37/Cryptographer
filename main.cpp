df72c64a3c981b9705cd4dcd5ab191d3ca6a316f
4elovek37 <b.4elovek37@yandex.ru>
2018-09-02 13:49:29 +0300

    Убрал измерения времени выполнения

--- main.cpp
+++ main.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <chrono>
-
-
 #include "include/Cryptographer.h"
 
 int main(int argc, char ** argv) {
-
-    std::chrono::time_point<std::chrono::system_clock> start, end;
-    start = std::chrono::system_clock::now();
-
 
     if(argc < 8) {
         std::cerr << "Invalid agrs...\n";
@@ -21,14 +14,17 @@ int main(int argc, char ** argv) {
     if(!iVfile.is_open())
         return 1;
 
-    crypto::primitives::Block iVblock(iVfile.tellg());
+    auto iVsize = iVfile.tellg();
+    if(iVsize <= 0)
+        return 1;
+
+    crypto::primitives::Block iVblock(static_cast<size_t>(iVsize));
 
     iVfile.seekg(0, iVfile.beg);
     iVfile.read(reinterpret_cast<char*>(iVblock.data_.data()), iVblock.data_.size());
     iVfile.close();
 
     unsigned long blockSize = std::stoul(argv[1]) / 8;
-
 
     std::ifstream inFile(argv[6], std::ifstream::binary);
     if(!inFile.is_open())
@@ -52,15 +48,8 @@ int main(int argc, char ** argv) {
         outFile.write(reinterpret_cast<char*>(block.data_.data()), i);
     }
 
-
     outFile.close();
     inFile.close();
-
-
-    end = std::chrono::system_clock::now();
-
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>
-            (end-start).count() << std::endl;
 
     return 0;
 }
